343be85ea47178df3d43528cc26da95aaa7245a3
Dante Allievi <danteallievi@gmail.com>
2021-11-30 21:09:50 +0100

    Added navbar to the imports on arreglalo.module

--- src/app/arreglalo.module.ts
+++ src/app/arreglalo.module.ts
@@ -5,9 +5,11 @@ import { ArreglaloRoutingModule } from "./arreglalo-routing.module";
 import { AppComponent } from "./arreglalo.component";
 import { ButtonComponent } from './components/button/button.component';
 import { CardComponent } from './components/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HamburgerComponent } from './components/navbar/hamburger/hamburger.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, CardComponent],
+  declarations: [AppComponent, ButtonComponent, CardComponent, NavbarComponent, HamburgerComponent],
   imports: [BrowserModule, ArreglaloRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
