10475adec6f9cc62dd7fb6414cd5bef44379803b
Dante Allievi <danteallievi@gmail.com>
2021-12-01 16:55:26 +0100

    Deleted useless constructor on RegisterComponent

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'arreglalo-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "arreglalo-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
