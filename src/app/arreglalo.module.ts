f00f60b95d8b6d5a06d431d4018d623b829abbe8
Dante Allievi <danteallievi@gmail.com>
2021-12-01 08:40:37 +0100

    Added HTTP module

--- src/app/arreglalo.module.ts
+++ src/app/arreglalo.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { ArreglaloRoutingModule } from "./arreglalo-routing.module";
 import { AppComponent } from "./arreglalo.component";
-import { ButtonComponent } from './components/button/button.component';
-import { CardComponent } from './components/card/card.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HamburgerComponent } from './components/navbar/hamburger/hamburger.component';
+import { ButtonComponent } from "./components/button/button.component";
+import { CardComponent } from "./components/card/card.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HamburgerComponent } from "./components/navbar/hamburger/hamburger.component";
+import { ListComponent } from "./components/list/list.component";
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, CardComponent, NavbarComponent, HamburgerComponent],
-  imports: [BrowserModule, ArreglaloRoutingModule],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    CardComponent,
+    NavbarComponent,
+    HamburgerComponent,
+    ListComponent,
+  ],
+  imports: [BrowserModule, ArreglaloRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
