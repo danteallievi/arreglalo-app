0959a59dfa4b2cd6a767a822dc364ac1cf8d2676
Dante Allievi <danteallievi@gmail.com>
2021-12-01 22:15:49 +0100

    Store service updated to add user observables

--- src/app/core/services/store/store.service.ts
+++ src/app/core/services/store/store.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, switchMap } from "rxjs";
+import { IUser } from "../../models/User";
 import { ArreglaloService } from "../arreglalo/arreglalo.service";
+import { UserService } from "../user/user.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class StoreService {
   public professionals$ = new BehaviorSubject<{}>({});
-
   public professionalsSubject$ = this.professionals$.pipe(
     switchMap(() => this.printProfessional())
   );
 
-  public constructor(private arreglaloService: ArreglaloService) {}
+  public user$ = new BehaviorSubject<IUser>({ email: "", password: "" });
+  public userSubject$ = this.user$.pipe(
+    switchMap((user) => this.loginUser(user))
+  );
+
+  public constructor(
+    private arreglaloService: ArreglaloService,
+    private userService: UserService
+  ) {}
 
   private printProfessional() {
     return this.arreglaloService.getProfessionals();
+  }
+
+  private loginUser(user: IUser) {
+    const { email, password } = user;
+
+    return this.userService.loginUserService(email, password);
   }
 }
