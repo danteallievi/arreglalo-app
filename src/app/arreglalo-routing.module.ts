8b8bafdce80e5e44de874bf426150d2126a5e2db
Dante Allievi <danteallievi@gmail.com>
2021-12-01 16:51:20 +0100

    Added to the routing module, register and list compontents

--- src/app/arreglalo-routing.module.ts
+++ src/app/arreglalo-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ListComponent } from "./components/list/list.component";
 import { LandingComponent } from "./pages/landing/landing.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/landing", pathMatch: "full" },
   { path: "landing", component: LandingComponent },
+  { path: "register", component: RegisterComponent },
   { path: "list", component: ListComponent },
 ];
 
