0c5af26c813b808dfa470a5187f53616d62a2ef7
Dante Allievi <danteallievi@gmail.com>
2021-12-01 23:36:17 +0100

    Added guards on routes

--- src/app/arreglalo-routing.module.ts
+++ src/app/arreglalo-routing.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ListComponent } from "./components/list/list.component";
-import { CheckLogGuard } from "./core/services/guard/checkLog.guard";
+import { ProtectRouteGuard } from "./core/services/guard/protect-route.guard";
+import { CheckAuthGuard } from "./core/services/guard/check-auth.guard";
 import { LandingComponent } from "./pages/landing/landing.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/landing", pathMatch: "full" },
   {
     path: "landing",
+    canActivate: [CheckAuthGuard],
     component: LandingComponent,
   },
   {
     path: "register",
+    canActivate: [CheckAuthGuard],
     component: RegisterComponent,
   },
-  { path: "login", component: LoginComponent },
-  { path: "list", canActivate: [CheckLogGuard], component: ListComponent },
+  { path: "login", canActivate: [CheckAuthGuard], component: LoginComponent },
+  { path: "list", canActivate: [ProtectRouteGuard], component: ListComponent },
+  { path: "**", pathMatch: "full", component: PageNotFoundComponent },
 ];
 
 @NgModule({
