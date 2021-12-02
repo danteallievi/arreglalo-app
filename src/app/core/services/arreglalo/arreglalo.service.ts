8e30c614455e6db3e361e371f139c1b6faba6fa3
Dante Allievi <danteallievi@gmail.com>
2021-12-02 16:45:29 +0100

    Create professional function created on the http service

--- src/app/core/services/arreglalo/arreglalo.service.ts
+++ src/app/core/services/arreglalo/arreglalo.service.ts
@@ -1,19 +1,26 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { IProfessional } from "../../models/Professional";
 
 @Injectable({
   providedIn: "root",
 })
 export class ArreglaloService {
-  apiUrl: string =
-    "https://proyecto-final-dante-back.herokuapp.com/professional";
+  apiUrl: string = "https://proyecto-final-dante-back.herokuapp.com/";
 
   constructor(private http: HttpClient) {}
 
   getProfessionals(): Observable<any> {
-    return this.http.get(this.apiUrl, {
+    return this.http.get(`${this.apiUrl}professional`, {
       headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     });
+  }
+
+  createProfessional(professional: IProfessional): Observable<any> {
+    return this.http.post(
+      `${this.apiUrl}user/professional/register`,
+      professional
+    );
   }
 }
