f4ab7838f1f08a4358a0df4a17a111d3086f5861
Dante Allievi <danteallievi@gmail.com>
2021-12-02 22:25:00 +0100

    Button component test added

--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -1,17 +1,96 @@
-import { TestBed } from "@angular/core/testing";
+// import { TestBed } from "@angular/core/testing";
+// import { ButtonComponent } from "./button.component";
+
+// describe("Given the button component", () => {
+//   describe("When it receives a text", () => {
+//     test("Then it should render the button with the given text", () => {
+//       const fixture = TestBed.createComponent(ButtonComponent);
+//       const app = fixture.componentInstance;
+//       app.buttonText = "Test";
+
+//       fixture.detectChanges();
+//       const compiled = fixture.debugElement.nativeElement;
+
+//       expect(compiled.querySelector("button").textContent).toContain("Test");
+//     });
+//   });
+// });
+
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
 import { ButtonComponent } from "./button.component";
 
-describe("Given the button component", () => {
-  describe("When it receives a text", () => {
-    test("Then it should render the button with the given text", () => {
-      const fixture = TestBed.createComponent(ButtonComponent);
-      const app = fixture.componentInstance;
-      app.buttonText = "Test";
+describe("Given the HamburgerComponent", () => {
+  let component: ButtonComponent;
+  let fixture: ComponentFixture<ButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [],
+      declarations: [ButtonComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  describe("When its rendered", () => {
+    it("Should created", () => {
+      expect(component).toBeTruthy();
+    });
+
+    it("Should have a method buttonOnClick", () => {
+      expect(component.buttonOnClick).toBeDefined();
+    });
+
+    it("Should raise a event when its clicked", () => {
+      const button = fixture.debugElement.nativeElement.querySelector("button");
+      jest.spyOn(component.buttonClick, "emit");
+
+      button.click();
+      expect(component.buttonClick.emit).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should raise a event when its clicked", () => {
+      const button = fixture.debugElement.nativeElement.querySelector("button");
+      jest.spyOn(component.buttonClick, "emit");
+
+      button.click();
+      expect(component.buttonClick.emit).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should have a primary color if it receives "primary" on buttonColor property', () => {
+      const button = fixture.debugElement.nativeElement.querySelector("button");
+      const expectedClass = "bg-primary";
+      const backgroundColorClass = button.classList;
+      component.buttonColor = "primary";
 
       fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
 
-      expect(compiled.querySelector("button").textContent).toContain("Test");
+      expect(backgroundColorClass["1"]).toBe(expectedClass);
+    });
+
+    it('Should have a secondary color if it receives "secondary" on buttonColor property', () => {
+      const button = fixture.debugElement.nativeElement.querySelector("button");
+      const expectedClass = "bg-secondary";
+      const backgroundColorClass = button.classList;
+      component.buttonColor = "secondary";
+
+      fixture.detectChanges();
+
+      expect(backgroundColorClass["1"]).toBe(expectedClass);
+    });
+
+    it("Should have a given text", () => {
+      const button = fixture.debugElement.nativeElement.querySelector("button");
+      const expectedText = "test";
+      component.buttonText = expectedText;
+
+      fixture.detectChanges();
+
+      expect(button.textContent).toContain(expectedText);
     });
   });
 });
