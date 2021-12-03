d187d1aee0a82459d52c7d5b07d871a42ddb1314
Dante Allievi <danteallievi@gmail.com>
2021-12-03 12:39:06 +0100

    Removed useless OnInit method

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "arreglalo-detail",
   templateUrl: "./detail.component.html",
   styleUrls: ["./detail.component.css"],
 })
-export class DetailComponent implements OnInit {
-  ngOnInit(): void {}
-}
+export class DetailComponent {}
