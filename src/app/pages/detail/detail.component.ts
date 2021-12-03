f901748255d2d2b2129c4d30700cf4293e617010
Dante Allievi <danteallievi@gmail.com>
2021-12-03 11:43:53 +0100

    Deleted useless constructor

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'arreglalo-detail',
-  templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  selector: "arreglalo-detail",
+  templateUrl: "./detail.component.html",
+  styleUrls: ["./detail.component.css"],
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
