0b0ab88fbbfabe0825e8d9e4b7952733fa9e594a
Dante Allievi <danteallievi@gmail.com>
2021-12-06 10:40:50 +0100

    Breaking test fixeds

--- src/app/components/page-not-found/page-not-found.spec.ts
+++ src/app/components/page-not-found/page-not-found.spec.ts
@@ -1,5 +1,6 @@
 import { RouterTestingModule } from "@angular/router/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 import { NavbarComponent } from "../navbar/navbar.component";
 import { PageNotFoundComponent } from "./page-not-found.component";
@@ -11,7 +12,7 @@ describe("HamburgerComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PageNotFoundComponent, NavbarComponent],
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
   });
 
--- src/app/core/services/store/store.service.spec.ts
+++ src/app/core/services/store/store.service.spec.ts
@@ -1,39 +1,60 @@
 import { TestBed } from "@angular/core/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { of } from "rxjs";
 
 import { StoreService } from "./store.service";
 
-describe("StoreService", () => {
-  let service: StoreService;
+let storeService: StoreService;
 
+const myProfessionals = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "test@test.com",
+  },
+];
+
+const StoreServiceMock = {
+  printProfessional: () => of(myProfessionals),
+  printCurrentProfessional: () => of(myProfessionals),
+  printVisitedProfessional: () => of(myProfessionals),
+};
+
+describe("StoreService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
-      providers: [StoreService],
+      providers: [{ provide: StoreService, useValue: StoreServiceMock }],
     });
-    service = TestBed.inject(StoreService);
+    storeService = TestBed.inject(StoreService);
   });
 
   it("should be created", () => {
-    expect(service).toBeTruthy();
+    expect(storeService).toBeTruthy();
   });
 
   it("should call printProfessional", () => {
-    const spyFn = spyOn(service, "printProfessional").and.callThrough();
-    service.printProfessional();
+    const spyFn = spyOn(storeService, "printProfessional").and.callThrough();
+    storeService.printProfessional();
     expect(spyFn).toHaveBeenCalled();
   });
 
   it("should call printCurrentProfessional", () => {
-    const spyFn = spyOn(service, "printCurrentProfessional").and.callThrough();
-    service.printCurrentProfessional();
+    const spyFn = spyOn(
+      storeService,
+      "printCurrentProfessional"
+    ).and.callThrough();
+    storeService.printCurrentProfessional();
     expect(spyFn).toHaveBeenCalled();
   });
 
   it("should call printVisitedProfessional", () => {
-    const spyFn = spyOn(service, "printVisitedProfessional").and.callThrough();
-    service.printVisitedProfessional("1");
+    const spyFn = spyOn(
+      storeService,
+      "printVisitedProfessional"
+    ).and.callThrough();
+    storeService.printVisitedProfessional("1");
     expect(spyFn).toHaveBeenCalled();
   });
 });
