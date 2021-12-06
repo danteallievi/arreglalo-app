8d8ffa2f53a26f30e54c71052f353db7c8ac3828
Dante Allievi <danteallievi@gmail.com>
2021-12-06 15:30:44 +0100

    Login component tested updated

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -3,12 +3,24 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { By } from "@angular/platform-browser";
+import { Observable } from "rxjs";
+
+import { PublicMethodsService } from "src/app/core/services/methods/public-methods.service";
 
 import { LoginComponent } from "./login.component";
 
 describe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let publicMethods: PublicMethodsService;
+
+  class PublicMethodsServiceMock {
+    loginUser = () =>
+      new Observable((observer) => {
+        observer.next(component.router.navigate(["/list"]));
+        observer.error((component.errorMessage = true));
+      });
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,6 +30,12 @@ describe("LoginComponent", () => {
         FormsModule,
         ReactiveFormsModule,
       ],
+      providers: [
+        {
+          provide: PublicMethodsService,
+          useClass: PublicMethodsServiceMock,
+        },
+      ],
       declarations: [LoginComponent],
     }).compileComponents();
   });
@@ -25,6 +43,8 @@ describe("LoginComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+
+    publicMethods = TestBed.inject(PublicMethodsService);
     fixture.detectChanges();
   });
 
@@ -77,5 +97,24 @@ describe("LoginComponent", () => {
     fixture.detectChanges();
 
     expect(component.onSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  it("Should call the public method if the submit was successfully", () => {
+    const spyFn = spyOn(publicMethods, "loginUser").and.callThrough();
+
+    const emailInput =
+      fixture.debugElement.nativeElement.querySelectorAll("input")[0];
+    const passwordInput =
+      fixture.debugElement.nativeElement.querySelectorAll("input")[1];
+    const form = fixture.debugElement.query(By.css("form"));
+    emailInput.value = "test@test.com";
+    passwordInput.value = "123456";
+    emailInput.dispatchEvent(new Event("input"));
+    passwordInput.dispatchEvent(new Event("input"));
+    form.triggerEventHandler("submit", null);
+
+    fixture.detectChanges();
+
+    expect(spyFn).toHaveBeenCalled();
   });
 });
