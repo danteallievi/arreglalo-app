4e3d79c4847a01b81cb001fab13610e80a2db6f8
Dante Allievi <danteallievi@gmail.com>
2021-12-07 19:20:06 +0100

    Stars component added on details page

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -29,6 +29,7 @@ export class DetailComponent implements OnInit {
 
   isLoadingThePage: boolean = false;
   isEditingProfile: boolean = false;
+  displayRateStars: boolean = false;
 
   myProfessional?: any | IProfessional;
   loggedUserData!: IUserLogged;
@@ -76,6 +77,8 @@ export class DetailComponent implements OnInit {
               city: [this.myProfessional.address.city, Validators.required],
               zipNumber: [this.myProfessional.address.zip, Validators.required],
             });
+
+            console.log(this.myProfessional.rate, "AAAAAAAAAAAa");
           },
           error: () => {
             this.router.navigate(["/not-found"]);
@@ -90,6 +93,7 @@ export class DetailComponent implements OnInit {
             this.isProfessionalHired = data.clients.includes(
               this.loggedUserData.id
             );
+            this.displayRateStars = true;
           },
           error: () => {
             this.router.navigate(["/not-found"]);
@@ -140,7 +144,12 @@ export class DetailComponent implements OnInit {
 
     this.publicMethods.updateCurrentProfesional(newProfessional).subscribe({
       complete: () => {
-        Swal.fire("Perfil editado correctamente.", "", "success");
+        Swal.fire({
+          title: "Perfil editado correctamente.",
+          icon: "success",
+          backdrop: `rgba(0,0,0,0.8)`,
+          confirmButtonColor: "#35615A",
+        });
         this.storeService.currentProfessional$.next({});
         this.router.navigate(["/detail"]);
         this.isEditingProfile = false;
@@ -172,13 +181,17 @@ export class DetailComponent implements OnInit {
       .hireSelectedProfessional(professionalToHireId)
       .subscribe({
         next: () => {
-          Swal.fire("Se ha contratado al profesional.", "", "success");
+          Swal.fire({
+            title: "Se ha contratado al profesional.",
+            icon: "success",
+            backdrop: `rgba(0,0,0,0.8)`,
+            confirmButtonColor: "#35615A",
+          });
         },
         error: () => {
           Swal.fire("Error contratando al profesional.", "", "error");
         },
         complete: () => {
-          Swal.fire("Se ha contratado al profesional.", "", "success");
           this.storeService.visitedProfessional$.next(this.userIdToDisplay);
           this.disabledButton = false;
         },
@@ -192,7 +205,12 @@ export class DetailComponent implements OnInit {
       .fireSelectedProfessional(professionalToFireId)
       .subscribe({
         next: () => {
-          Swal.fire("Se ha despedido al profesional.", "", "success");
+          Swal.fire({
+            title: "Se ha despedido al profesional.",
+            icon: "success",
+            backdrop: `rgba(0,0,0,0.8)`,
+            confirmButtonColor: "#35615A",
+          });
         },
         error: () => {
           Swal.fire("Error despidiendo al profesional.", "", "error");
@@ -200,6 +218,21 @@ export class DetailComponent implements OnInit {
         complete: () => {
           this.storeService.visitedProfessional$.next(this.userIdToDisplay);
           this.disabledButton = false;
+        },
+      });
+  }
+
+  handleRatingClick(numberOfRate: number, professionalToHireId: string) {
+    this.publicMethods
+      .rateSelectedProfessional(professionalToHireId, numberOfRate)
+      .subscribe({
+        next: () => {
+          Swal.fire({
+            title: "Se ha calificado al profesional.",
+            icon: "success",
+            backdrop: `rgba(0,0,0,0.8)`,
+            confirmButtonColor: "#35615A",
+          });
         },
       });
   }
